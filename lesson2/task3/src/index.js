09c8a9d5e5f73d718fc4c51cb6d5d8788746463b
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-18 21:52:23 +0300

    task 4

---
+++ lesson2/task3/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const render = (time) => {
+  const rootEl = document.querySelector('#root');
+  const seconds = new Date(time).getSeconds();
+
+  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
+  const textColor = seconds % 2 === 0 ? '#000' : '#fff';
+  const styles = {
+    color: textColor,
+    backgroundColor,
+  };
+
+  const element = (
+    <div className="seconds" style={styles}>
+      {seconds}
+    </div>
+  );
+
+  ReactDOM.render(element, rootEl);
+};
+
+render(new Date());
+
+setInterval(() => render(new Date()), 1000);
