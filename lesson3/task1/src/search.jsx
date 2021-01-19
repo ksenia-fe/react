67b2c2ab87dd928a49bca48a3b822c711584c90c
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-19 15:41:16 +0300

    test

---
+++ lesson3/task1/src/search.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.scss';
+import './search.scss';
+
+export default (props) => {
+    const element = (
+        <div className="search">
+            <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+            <div className="search__field">
+                <input type="text" className="search__input" />
+                <button className="search__button">Search</button>
+            </div>
+        </div>
+    )
+
+    return element;
+}
