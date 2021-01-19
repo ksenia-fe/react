b860646e896d552a3a54839fe0fe5bc981455838
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-19 15:43:43 +0300

    test

--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Search from './search.jsx'
+import Search from './Search.jsx'
 import './index.scss';
 import './search.scss';
 
