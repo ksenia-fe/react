e0a1bb247a6b445858cb3fcbc1012b117d41d59e
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-19 15:45:12 +0300

    d

--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Search from './Search.jsx'
+import Search from './search.jsx'
 import './index.scss';
 import './search.scss';
 
 const rootEl = document.querySelector('#root');
 
-ReactDOM.render(<Search name="Xu"/>, rootEl);
+ReactDOM.render(<Search name="Tom"/>, rootEl);
