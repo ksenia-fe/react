aa6d44061a2ce750c223d271c412f01f39044842
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-20 10:32:45 +0300

    renamed Avatar doc

--- lesson3/task3.1/src/UserInfo.jsx
+++ lesson3/task3.1/src/UserInfo.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import UserAvatar from './UserAvatar.jsx'
+import UserAvatar from './Avatar.jsx'
 
 function UserInfo(props) {
     return (
         <div className="user-info">
           <UserAvatar 
             avatartUrl={props.user.avatartUrl} 
-            user={props.user.name}/>
+            user={props.user.name}
+          />
           <div className="user-info__name">{props.user.name}</div>
         </div>
     );
