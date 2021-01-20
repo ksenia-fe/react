59de7948485f5cf0abc7091ac05f522e583cf76c
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-20 20:24:57 +0300

    moment lib

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
+import moment from 'moment';
 
 const Profile = (props) => {
-    const formatted = new Intl.DateTimeFormat('en',
-         { month: 'short',
-           day: '2-digit',
-           year: '2-digit',
-           formatMatcher: 'basic'
-        }).format(new Date(props.userData.birthDate)).replace(/,/g,'');
-    const y = [...formatted];
-    [y[0], y[1], y[2],y[3],y[4],y[5]] = [y[4],y[5],y[3], y[0], y[1],y[2]];
+    const date = moment(new Date(props.userData.birthDate)).format('MMM DD YY');
 
     return (
         <div className='profile'>
             <div className='profile__name'>{`${props.userData.firstName} ${props.userData.lastName}`}</div>
-            <div className='profile__birth'>{`Was born ${y.join('')} in ${props.userData.birthPlace}`}</div>
+            <div className='profile__birth'>{`Was born ${date} in ${props.userData.birthPlace}`}</div>
         </div>
     )
 }
