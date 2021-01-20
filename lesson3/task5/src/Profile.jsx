39cf2bd1d546c7582c0b1b14560f9fe43cbd9f89
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-20 20:04:06 +0300

    v

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 const Profile = ({firstName, lastName, birthDate, birthPlace}) => {
-    const formatter = new Intl.DateTimeFormat('en',
+    const formatted = new Intl.DateTimeFormat('en',
          { month: 'short',
            day: '2-digit',
            year: '2-digit',
            formatMatcher: 'basic'
         }).format(new Date(birthDate)).replace(/,/g,'');
-    const y = [...formatter];
+    const y = [...formatted];
     [y[0], y[1], y[2],y[3],y[4],y[5]] = [y[4],y[5],y[3], y[0], y[1],y[2]];
-    console.log(y);
-        // Jan_17_91
+
     return (
         <div className='profile'>
             <div className='profile__name'>{`${firstName} ${lastName}`}</div>
