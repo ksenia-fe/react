54d650a079bbb6071a4948bd93daf40d2726c4f2
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-20 20:07:55 +0300

    p

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const Profile = ({firstName, lastName, birthDate, birthPlace}) => {
+const Profile = (userData) => {
     const formatted = new Intl.DateTimeFormat('en',
          { month: 'short',
            day: '2-digit',
            year: '2-digit',
            formatMatcher: 'basic'
-        }).format(new Date(birthDate)).replace(/,/g,'');
+        }).format(new Date(userData.birthDate)).replace(/,/g,'');
     const y = [...formatted];
     [y[0], y[1], y[2],y[3],y[4],y[5]] = [y[4],y[5],y[3], y[0], y[1],y[2]];
 
     return (
         <div className='profile'>
-            <div className='profile__name'>{`${firstName} ${lastName}`}</div>
-            <div className='profile__birth'>{`${y.join('')} ${birthPlace}`}</div>
+            <div className='profile__name'>{`${userData.firstName} ${userData.lastName}`}</div>
+            <div className='profile__birth'>{`Was born ${y.join('')} ${userData.birthPlace}`}</div>
         </div>
     )
 }
