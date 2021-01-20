44d5ac73501825c9337c9947f751e828303e13f3
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-20 21:37:52 +0300

    task1

---
+++ lesson4/task1/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Counter from './Counter.jsx'
+
+const App = () => {
+ return (
+     <>
+    <Counter 
+        start={17}
+        interval={1000}
+    />
+    <Counter 
+        start={3}
+        interval={100}
+    />
+    <Counter 
+        start={11}
+        interval={3000}
+    />
+    </>
+ );
+}
+
+export default App
