56e5f81351d5d940b4f85b5d8c0d18e69bf7a55c
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-21 14:53:46 +0300

    с

--- lesson5/task5/src/Toggler.jsx
+++ lesson5/task5/src/Toggler.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 class Toggler extends React.Component {
-    toggleHandler = (e) => e.target.textContent === 'Off' ? e.target.textContent = 'On' : e.target.textContent = 'Off'
+    toggleHandler = (e) => e.target.innerText === 'Off' ? e.target.innerText = 'On' : e.target.innerText = 'Off'
     render(){
         return(
             <div className="toggler"
-            onClick={this.toggleHandler}>Off</div>
+                onClick={this.toggleHandler}>Off
+            </div>
         )
     }
 }
