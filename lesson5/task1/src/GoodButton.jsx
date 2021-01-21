adc7fbc21fa3fabcc9392aaf4b97fefc0711acbf
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-21 13:42:48 +0300

    syntetic e

--- lesson5/task1/src/GoodButton.jsx
+++ lesson5/task1/src/GoodButton.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const GoodButton = () => (
-        <button className="fancy-button"
-            onClick={() => alert('Good job!') }>
+// task 1
+// const GoodButton = () => (
+//         <button className="fancy-button"
+//             onClick={() => alert('Good job!') }>
+//             Click me!
+//         </button>
+// );
+
+// task 2 
+class GoodButton extends Component {
+    handleClick(event){
+        alert(event.target.textContent);
+    }
+    render(){
+        return (
+            <button className="fancy-button"
+            onClick={this.handleClick}>
             Click me!
         </button>
-    );
-
+        )
+    }
+}
 
 export default GoodButton
