41a7c5821924efd905a4b94ae7952b057854375a
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-21 14:25:57 +0300

    three buttons rgb

---
+++ lesson5/task4/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Colors from './Colors.jsx';
+
+const rootEl = document.querySelector('#root');
+
+ReactDOM.render(<Colors/>, rootEl);
