ae0eff2b39f6f96bc7a15e6d35641a0f35393bc3
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-23 14:00:48 +0300

    rendering cancel

---
+++ lesson6/task3/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Page from './Page.jsx'
+
+const rootEl = document.querySelector('#root');
+
+ReactDOM.render(<Page/>, rootEl);
