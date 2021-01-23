ce9657f442b8d53b0a8ef03e43704e3c4ff86311
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-23 23:22:52 +0300

    date

--- lesson7/task3/src/Transaction.jsx
+++ lesson7/task3/src/Transaction.jsx
@@ -4,13 +4,13 @@ import moment from 'moment';
 const Transaction = ({ from, to, amount, rate, time}) => {
     const amountFormat = new Intl.NumberFormat('en-GB').format(amount);
     const formattedTime = moment(time).format('HH:mm');
-    const date = moment(time).format('MMM YY')
+    const date = moment(time).format('DD MMM')
 
     return (
         <li className="transaction">
             <span className="transaction__date">{date}</span>
             <span className="transaction__time">{formattedTime}</span>
-            <span className="transaction__assets">{`${from} → ${to}`} </span>
+            <span className="transaction__assets">{`${from} → ${to}`}</span>
             <span className="transaction__rate">{rate}</span>
             <span className="transaction__amount">{amountFormat}</span>
         </li>
