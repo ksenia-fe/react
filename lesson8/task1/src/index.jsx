9c0a90977740789fdb29e1afb62a352a1514043e
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-24 10:53:03 +0300

    8.1

---
+++ lesson8/task1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Life from './Life.jsx'
+
+const rootEl = document.querySelector('#root');
+
+ReactDOM.render(<Life />, rootEl);
