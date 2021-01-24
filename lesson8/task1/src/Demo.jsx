d7edee1e9cb384a574b80b5a446da15d85bd4f34
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-24 13:21:18 +0300

    x

--- lesson8/task1/src/Demo.jsx
+++ lesson8/task1/src/Demo.jsx
@@ -1,49 +1,49 @@
-import React from 'react';
-import Life from './Life.jsx';
+// import React from 'react';
+// import Life from './Life.jsx';
 
-class Demo extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            number: Math.round(Math.random() * 100)
-        }
-        console.log('consctructor: good place to create state');
-    }
+// class Demo extends React.Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             number: Math.round(Math.random() * 100)
+//         }
+//         console.log('consctructor: good place to create state');
+//     }
 
-    show = () => {
-        this.setState({
-            visible: true
-        });
-    }
-    hide = () => {
-        this.setState({
-            visible: false
-        });
-    }
-    update = () => {
-        this.setState({
-            number: Math.round(Math.random() * 100)
-        });
-    }
+//     show = () => {
+//         this.setState({
+//             visible: true
+//         });
+//     }
+//     hide = () => {
+//         this.setState({
+//             visible: false
+//         });
+//     }
+//     update = () => {
+//         this.setState({
+//             number: Math.round(Math.random() * 100)
+//         });
+//     }
 
-    render() {
-        return (
-            <div>
-                <div>
-                    <button className="btn" onClick={this.show}>
-                        Show
-                    </button>
-                    <button className="btn" onClick={this.hide}>
-                        Hide
-                    </button>
-                    <button className="btn" onClick={this.update}>
-                        Update
-                    </button>
-                    {this.state.visible && <Life number={this.state.number}/>}
-                </div>
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 <div>
+//                     <button className="btn" onClick={this.show}>
+//                         Show
+//                     </button>
+//                     <button className="btn" onClick={this.hide}>
+//                         Hide
+//                     </button>
+//                     <button className="btn" onClick={this.update}>
+//                         Update
+//                     </button>
+//                     {this.state.visible && <Life number={this.state.number}/>}
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
-export default Demo;
+// export default Demo;
--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 
 class Life extends React.Component {
-    constructor(props){
-        super(props);
-        console.log('consctructor: good place to create state');
+    constructor(props) {
+      super(props);
+      console.log('constructor: good place to create state');
     }
-
+  
     componentDidMount() {
-        console.log('componentDidMount: API calls, subscriptions');
+      console.log('componentDidMount: API calls, subscriptions');
     }
-
+  
     componentWillUnmount() {
-        console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
+      console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
     }
-
+  
     shouldComponentUpdate() {
-        console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-        return true;
+      console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+      return true;
     }
-
+  
     componentDidUpdate() {
-        console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
+      console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
     }
-
+  
     render() {
-        console.log('return React element to build DOM');
-        return (
-            <div>{this.props.number}</div>
-        )
+      console.log('return React element to build DOM');
+      return (
+        <div>{this.props.number}</div>
+    )
     }
-}
+  }
 
 export default Life;
