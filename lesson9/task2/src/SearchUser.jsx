854d03e80b8de8baa95a0c99660fed4ad690f6d7
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-25 15:43:47 +0300

    8.2

---
+++ lesson9/task2/src/SearchUser.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+
+class SearchUser extends React.Component {
+  state = {
+    name: '',
+    student: '',
+    occupation: '',
+    about: '',
+  };
+  handleChange = (event) => {
+    const { name, value, checked, type } = event.target;
+
+    const val = type === 'checkbox' ? checked : value;
+    this.setState({
+      [name]: val,
+    });
+  };
+
+  submit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <form className="login-form" onSubmit={this.submit}>
+        <h1 className="form-title">Profile</h1>
+        <div className="form-control">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input
+            value={this.state.name}
+            onChange={this.handleChange}
+            className="form-input"
+            type="text"
+            id="name"
+            name="name"
+          />
+        </div>
+        <div className="form-control">
+          <label className="form-label" htmlFor="student">
+            Student
+          </label>
+          <input
+            value={this.state.student}
+            onChange={this.handleChange}
+            className="form-input"
+            type="checkbox"
+            id="student"
+            name="student"
+          />
+        </div>
+        <div className="form-control">
+          <label className="form-label" id="occupation" htmlFor="occupation">
+            Occupation
+          </label>
+          <select
+            value={this.state.occupation}
+            onChange={this.handleChange}
+            name="occupation"
+            className="form-input"
+          >
+            <option value="london">London</option>
+            <option value="new-york">New York</option>
+            <option value="sidney">Sidney</option>
+            <option value="berlin">Berlin</option>
+          </select>
+        </div>
+        <div className="form-control">
+          <label className="form-label" id="about" htmlFor="about">
+            About
+          </label>
+          <textarea
+            name="about"
+            className="form-input"
+            value={this.state.about}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button className="submit-button" type="submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+export default SearchUser;
---
+++ lesson9/task2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import SearchUser from './SearchUser.jsx';
+
+const rootEl = document.querySelector('#root');
+
+ReactDOM.render(<SearchUser />, rootEl);
