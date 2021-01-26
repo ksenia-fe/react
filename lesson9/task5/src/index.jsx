180f440d52ed4eca3ab4a9c49b29d6c64e0461e3
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-26 16:03:50 +0300

    tic-tac-toe

---
+++ lesson9/task5/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './index.js';
+
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(<Game />, rootEl);
