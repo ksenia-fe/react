7e51f16bdae3b25b956259f0c3d48110733815f4
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-26 13:46:53 +0300

    refactoring

--- lesson5/task6/src/ColorPicker.jsx
+++ lesson5/task6/src/ColorPicker.jsx
@@ -4,41 +4,42 @@ class ColorPicker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: null,
+      title: null,
     };
   }
 
   setColorText = (color) => {
     this.setState({
-      text: color,
+      title: color,
     });
   };
 
-  empty = () => {
+  setTitle = (title) => {
     this.setState({
-      text: (this.state.text = ''),
+      title,
     });
   };
+  clearTitle = () => this.setTitle(null);
 
   render() {
     return (
       <div>
-        <div className="picker__title">{this.state.text}</div>
+        <div className="picker__title">{this.state.title}</div>
         <div>
           <button
             className="picker__button picker__button_coral"
             onMouseOver={() => this.setColorText('Coral')}
-            onMouseOut={this.empty}
+            onMouseOut={this.clearTitle}
           ></button>
           <button
             className="picker__button picker__button_aqua"
             onMouseOver={() => this.setColorText('Aqua')}
-            onMouseOut={this.empty}
+            onMouseOut={this.clearTitle}
           ></button>
           <button
             className="picker__button picker__button_bisque"
             onMouseOver={() => this.setColorText('Bisque')}
-            onMouseOut={this.empty}
+            onMouseOut={this.clearTitle}
           ></button>
         </div>
       </div>
--- lesson9/task4/src/UsersList.jsx
+++ lesson9/task4/src/UsersList.jsx
@@ -3,13 +3,10 @@ import User from './User.jsx';
 import Filter from './Filter.jsx';
 
 class UsersList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 4,
-      filterText: '',
-    };
-  }
+  state = {
+    count: 4,
+    filterText: '',
+  };
 
   inputHandler = (event) => {
     this.setState({
@@ -27,7 +24,6 @@ class UsersList extends React.Component {
         />
         <ul className="users">
           {this.props.users
-            .slice()
             .map((user) => <User key={user.id} {...user} />)
             .filter((user) =>
               user.props.name
