fc735d3dff227d6bf47052b54fe22eebb0431ad1
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-27 18:28:49 +0300

    reorganized

--- lesson11/task3/src/App.jsx
+++ lesson11/task3/src/App.jsx
@@ -2,29 +2,10 @@ import React from 'react';
 import Expand from './Expand';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false,
-    };
-  }
-
-  contentToggler = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  };
-
   render() {
-    const arrow = this.state.isOpen === true ? '↑' : '↓';
     return (
       <div className="app">
-        <Expand
-          arrow={arrow}
-          isOpen={this.state.isOpen}
-          toggler={this.contentToggler}
-          title={'this is title'}
-        >
+        <Expand title={'this is title'}>
           <p>
             Hooks are a new addition in React 16.8. They let you use state and
             other React features without writing a class.
--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 
 class Expand extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  contentToggler = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
   render() {
     return (
       <div className="expand border">
         <div className="expand__header">
           <span className="expand__title">{this.props.title}</span>
-          <button onClick={this.props.toggler} className="expand__toggle-btn">
-            {this.props.arrow}
+          <button onClick={this.contentToggler} className="expand__toggle-btn">
+            {this.state.isOpen === true ? '↑' : '↓'}
           </button>
         </div>
-        {this.props.isOpen ? (
+        {this.state.isOpen ? (
           <div className="expand__content">{this.props.children}</div>
         ) : null}
       </div>
