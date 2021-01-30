0da6a1674051eb6e7e2c64fa2f0926ecdabadf9b
Mona Lisa <kseniabelikova35@gmail.com>
2021-01-30 15:27:44 +0300

    use Effect hook

---
+++ lesson14/task2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import App from './App';
+
+const rootEl = document.querySelector('#root');
+
+ReactDOM.render(<App />, rootEl);
