6b4202e26e6d23dea70e102f102e5df069a54f38
Mona Lisa <kseniabelikova35@gmail.com>
2021-02-02 14:16:40 +0300

    .

--- live/react/pagination/src/Pagination.jsx
+++ live/react/pagination/src/Pagination.jsx
@@ -7,15 +7,18 @@ const Pagination = ({
   totalItems,
   itemsPerPage,
 }) => {
-  let isPrevPageAvailable = currentPage > 1;
-  let isNextPageAvailable = currentPage < Math.ceil(totalItems / itemsPerPage); // in my case it shoult be less than 2
+  const isPrevPageAvailable = currentPage > 1;
+  const isNextPageAvailable =
+    currentPage < Math.ceil(totalItems / itemsPerPage); // in my case it shoult be less than 2
 
   return (
     <div className="pagination">
       <button className="btn" onClick={goPrev} disabled={!isPrevPageAvailable}>
         {isPrevPageAvailable && '←'}
       </button>
+
       <span className="pagination__page">{currentPage}</span>
+
       <button className="btn" onClick={goNext} disabled={!isNextPageAvailable}>
         {isNextPageAvailable && '→'}
       </button>
