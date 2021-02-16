49d4250527eccefd50a127d4272ba977bba23934
Mona Lisa <kseniabelikova35@gmail.com>
2021-02-16 12:28:29 +0300

    .

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
 
 class Life extends React.Component {
-    constructor(props) {
-      super(props);
-      console.log('constructor: good place to create state');
-    }
-  
-    componentDidMount() {
-      console.log('componentDidMount: API calls, subscriptions');
-    }
-  
-    componentWillUnmount() {
-      console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
-    }
-  
-    shouldComponentUpdate() {
-      console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-      return true;
-    }
-  
-    componentDidUpdate() {
-      console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
-    }
-  
-    render() {
-      console.log('return React element to build DOM');
-      return (
-        <div>{this.props.number}</div>
-    )
-    }
+  constructor(props) {
+    super(props);
+    console.log('=> constructor: good place to create state');
   }
+
+  componentDidMount() {
+    console.log('=> componentDidMount: API calls, subscriptions');
+  }
+
+  componentWillUnmount() {
+    console.log(
+      '=> componentWillUnmount(): cleanup before DOM related to component will be removed'
+    );
+  }
+
+  shouldComponentUpdate(nextProps) {
+    // before render
+    console.log('nextProps => ', nextProps); // те props, что сейчас
+    console.log(
+      '=> shouldComponentUpdate(nextProps, nextState): decide to render or not to render'
+    );
+    return true;
+  }
+
+  componentDidUpdate(prevProps) {
+    // after render
+    console.log('prevProps => ', prevProps); // те props,что были до этого
+    console.log(
+      '=> componentDidUpdate(prevProps, prevState): some updates based on new props'
+    );
+  }
+
+  render() {
+    console.log('=> render - return React element to build DOM');
+    return <div>{this.props.number}</div>;
+  }
+}
 
 export default Life;
--- lesson8/task1/src/index.jsx
+++ lesson8/task1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Life from './Life.jsx'
+import Demo from './Demo.jsx';
 
 const rootEl = document.querySelector('#root');
 
-ReactDOM.render(<Life number={42}/>, rootEl);
+ReactDOM.render(<Demo number={42} />, rootEl);
